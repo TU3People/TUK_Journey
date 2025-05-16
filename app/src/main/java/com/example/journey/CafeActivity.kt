a556588e4220323232e590a901383af4b9fb72b5
bana1127 <jaeson1012@naver.com>
2025-05-16 14:36:21 +0900

    0516

---
+++ app/src/main/java/com/example/journey/CafeActivity.kt
@@ -0,0 +1,26 @@
+package com.example.journey
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.journey.databinding.ActivityCafeBinding
+
+class CafeActivity : AppCompatActivity() {
+    lateinit var binding : ActivityCafeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityCafeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
